9aa4ca6d40b1e07df9c3cba65deade87afd7324c
rafaelbau34 <rafaelbaugar@gmail.com>
2025-07-31 01:20:33 -0600

    DOM used instead of innerHTML, url fixed and weather feels like added

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ const searchBtn = document.getElementById("searchBtn");
 
 let lastWeatherData = null;
 
+const cityName = document.createElement("h2");
+
+const temperature = document.createElement("p");
+
+const cond = document.createElement("p");
+
+const feels = document.createElement("p");
+
+output.appendChild(cityName);
+output.appendChild(temperature);
+output.appendChild(cond);
+output.appendChild(feels);
+
 async function searchCity(city) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}
-      ?key=UZNCUXBJNV6N9DJMKYP4WKXE9`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=UZNCUXBJNV6N9DJMKYP4WKXE9`,
       {
         mode: "cors",
       }
@@ -32,12 +44,17 @@ async function getData(city) {
   const data = await searchCity(city);
   const f = data.currentConditions.temp;
   const c = toC(f);
+  const feels = data.currentConditions.feelslike;
+  const translated = toC(feels);
 
   return {
     location: data.resolvedAddress,
     conditions: data.currentConditions.conditions,
+    feels: data.currentConditions.feelslike,
     tempF: f.toFixed(1),
     tempC: c.toFixed(1),
+    feelF: feels.toFixed(1),
+    feelC: translated.toFixed(),
   };
 }
 
@@ -49,11 +66,19 @@ function updateDisplay() {
     unit === "c"
       ? `${lastWeatherData.tempC} C°`
       : `${lastWeatherData.tempF} F°`;
-  output.innerHTML = `
-        <h2>${lastWeatherData.location}</h2>
-        <p>Temperature: ${temp}</p>
-        <p>Conditions: ${lastWeatherData.conditions}</p>
-        `;
+
+  const feelTemp =
+    unit === "c"
+      ? `Feels like ${lastWeatherData.feelC} C°`
+      : `Feels like ${lastWeatherData.feelF} F°`;
+
+  cityName.textContent = lastWeatherData.location;
+
+  temperature.textContent = temp;
+
+  cond.textContent = lastWeatherData.conditions;
+
+  feels.textContent = feelTemp;
 }
 
 searchBtn.addEventListener("click", async () => {
